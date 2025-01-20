38ff02114097a1bfe877bd8a91650e1812f27e13
sdputurn <sdp.uturn@gmail.com>
2025-01-20 14:07:15 +0530

    fix module import issue

--- go-examples/main.go
+++ go-examples/main.go
@@ -1,6 +1,8 @@
 package main
 
-import http_request "http-request"
+import (
+	http_request "github.com/sdputurn/k8s-admission-webhook-and-cabundle-guide/http-request"
+)
 
 func main() {
 	http_request.HTTPrequest()
